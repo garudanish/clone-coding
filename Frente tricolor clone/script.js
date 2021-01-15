bc7ac73876929ffb833ac1f284748d5aa73a066c
HyeonsuKim <pkj3186@naver.com>
2021-01-15 17:40:05 +0900

    FT Clone: Complete Header part

--- Frente tricolor clone/script.js
+++ Frente tricolor clone/script.js
@@ -1,10 +1,16 @@
 const HEADER_TOP = "header--top";
 const MOBILE_CLICKED = "mobile--clicked";
+const SHOWING = "showing";
 
 const header = document.querySelector(".header");
-const menu = document.querySelector(".header__menu");
+const userMenu = document.querySelector(".header__user");
+const navMenu = document.querySelector(".header__nav");
 const hamberger = document.querySelector(".fa-bars");
 const closeMenu = document.querySelector(".fa-times");
+const depth1Menu = document.querySelector(
+  ".header__nav__depth1 li:nth-child(3)"
+);
+const subMenu = document.querySelector(".header__nav__depth2");
 
 function changeHeaderBG() {
   if (window.pageYOffset < 200) {
@@ -24,10 +30,12 @@ handleHeaderBG();
 function toggleMenu() {
   if (header.classList.contains(MOBILE_CLICKED)) {
     header.classList.remove(MOBILE_CLICKED);
-    menu.classList.remove(MOBILE_CLICKED);
+    userMenu.classList.remove(MOBILE_CLICKED);
+    navMenu.classList.remove(MOBILE_CLICKED);
   } else {
     header.classList.add(MOBILE_CLICKED);
-    menu.classList.add(MOBILE_CLICKED);
+    userMenu.classList.add(MOBILE_CLICKED);
+    navMenu.classList.add(MOBILE_CLICKED);
   }
 }
 
@@ -37,3 +45,18 @@ function handleMenu() {
 }
 
 handleMenu();
+
+function paintSubMenu() {
+  subMenu.classList.add(SHOWING);
+}
+
+function handleSubMenu() {
+  depth1Menu.addEventListener("mouseenter", function () {
+    subMenu.classList.add(SHOWING);
+  });
+  depth1Menu.addEventListener("mouseleave", function () {
+    subMenu.classList.remove(SHOWING);
+  });
+}
+
+handleSubMenu();
