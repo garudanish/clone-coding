29eb2ec69fac420f0a470736aa712cb2dd0dd67f
HyeonsuKim <pkj3186@naver.com>
2021-01-15 22:44:39 +0900

    FT Clone: Add carousel button function

--- Frente tricolor clone/carousel.js
+++ Frente tricolor clone/carousel.js
@@ -1,5 +1,8 @@
 const SHOWING_CLASS = "showing";
 const firstSlide = document.querySelector(".main__slider--slide:first-child");
+const lastSlide = document.querySelector(".main__slider--slide:last-child");
+const prevButton = document.querySelector(".fa-chevron-circle-left");
+const nextButton = document.querySelector(".fa-chevron-circle-right");
 
 function slide() {
   const currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
@@ -17,5 +20,23 @@ function slide() {
 }
 
 slide();
-
 setInterval(slide, 5000);
+
+nextButton.addEventListener("click", slide);
+
+function clickPrevButton() {
+  const currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
+  if (currentSlide) {
+    currentSlide.classList.remove(SHOWING_CLASS);
+    const prevSlide = currentSlide.previousElementSibling;
+    if (prevSlide) {
+      prevSlide.classList.add(SHOWING_CLASS);
+    } else {
+      lastSlide.classList.add(SHOWING_CLASS);
+    }
+  } else {
+    lastSlide.classList.add(SHOWING_CLASS);
+  }
+}
+
+prevButton.addEventListener("click", clickPrevButton);
